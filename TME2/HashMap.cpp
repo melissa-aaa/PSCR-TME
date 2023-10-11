479778b040da03cc697ec250b1be62defa2591df
Mélissa Lacour <Melissa.Lacour@etu.sorbonne-universite.fr>
2023-10-11 18:59:16 +0200

    rendu tme2 !

---
+++ TME2/HashMap.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <chrono>
+#include <string>
+#include "HashMap.h"
+
+int main(){ 
+    using namespace pr;
+    using namespace std;
+
+    /*
+    //tests de la classe HashMap indépendants 
+    size_t capacite = 10;
+
+    //HashMap<string,int> h(capacite);
+
+    HashMap<string,int> *h = new HashMap<string,int>(capacite);
+
+    for(size_t i = 0; i < capacite*4 ; i++) {
+        
+        pair<string,int> *p = new pair<string,int>(to_string(i),i);
+        h->put(p->first,p->second);
+        cout << h->get(to_string(i)) << endl;
+    }
+
+    h->put("1", 32);
+
+    cout << "Valeur modifiée : " << h->get("1") << endl;
+    */
+
+    //Quesion 6 
+
+    // Crée une instance de la classe HashMap pour stocker les occurrences des mots
+    HashMap<string, int> occurrences(100); 
+
+    ifstream input = ifstream("WarAndPeace.txt");
+
+    if (!input.is_open()) {
+        cerr << "Erreur : Impossible d'ouvrir le fichier." << endl;
+        return 1;
+    }
+
+    //mot lu 
+    string word;
+
+    //reg expression qui reconnaît les caractères anormaux 
+    regex re( R"([^a-zA-Z])");
+
+    while (input >> word) {
+        // élimine la ponctuation et les caractères spéciaux
+		word = regex_replace ( word, re, "");
+		// passe en lowercase
+		transform(word.begin(),word.end(),word.begin(),::tolower);
+
+		// word est maintenant "tout propre"
+        // Vérifie si le mot existe déjà dans la table de hachage
+        if (occurrences.existe(word)) {
+            // Le mot existe, donc augmentez le compteur d'occurrences
+            int cpt = occurrences.get(word);
+            occurrences.put(word, cpt + 1);
+        } else {
+            // Le mot n'existe pas, donc ajoutez-le avec un compteur initialisé à 1
+            occurrences.put(word, 1);
+        }
+    }
+
+    // Affichez le nombre d'occurrences des mots spécifiques (war, peace, toto)
+    cout << "Occurrences de 'war': " << occurrences.get("war") << endl;
+    cout << "Occurrences de 'peace': " << occurrences.get("peace") << endl;
+    cout << "Occurrences de 'toto': " << occurrences.get("toto") << endl;
+
+    // Vous pouvez également afficher d'autres statistiques comme le nombre total de mots différents, etc.
+
+   //Question 7 
+    // Crée un vecteur pour stocker les entrées de la table de hachage
+    vector<pair<string, int>> vectorHMap = occurrences.getAllPairs();
+
+
+    cout << "Taille de vectorHMap : " << vectorHMap.size() << endl;
+
+    // ici, vectorFromHashMap contient une copie des entrées de la table de hachage
+
+   // Question 8 
+    sort(vectorHMap.begin(), vectorHMap.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
+        return a.second > b.second;
+    });
+
+    /* 
+    //afficher toutes les entrées du vecteur  
+
+    for (const auto& entry : vectorHMap) {
+        cout << "Clé : " << entry.first << ", Valeur : " << entry.second << endl;
+    }
+    */
+
+    // Afficher les dix mots les plus fréquents
+    cout << "Les dix mots les plus fréquents : " << endl;
+    int numMotsAffiches = min(10, static_cast<int>(vectorHMap.size())); // Le nombre réel de mots à afficher
+    for (int i = 0; i < numMotsAffiches; ++i) {
+        cout << vectorHMap[i].first << " : " << vectorHMap[i].second << " occurrences" << endl;
+    }
+    
+    input.close();
+
+    return 0;
+
+}
---
+++ TME2/HashMap.h
@@ -0,0 +1,111 @@
+#ifndef HASH_MAP_H
+#define HASH_MAP_H
+
+#include <iostream>   
+#include <vector>
+#include <forward_list>
+#include <utility>
+#include <string>
+#include <functional>
+
+namespace pr {
+	using namespace std;
+
+	template<typename K, typename V>
+
+	class HashMap{
+		size_t taille; // Taille de la table de hachage (nombre d'éléments)
+		size_t capaciteMax; // nombre d'éléments max de la table de hachage
+		std::vector<std::forward_list<std::pair<K,V>>> tabHash; //vecteur de listes chaînées pour stocker les éléments
+		//forward_liste est un parcours -> mais consomme moins de mémoire 
+		
+
+        // Fonction de hachage basique pour obtenir un index dans le tableau
+        size_t hash(const K& key) const {
+            //on crée un objet de hashage (fonction standard fournie par C++) 
+            return std::hash<K>{}(key) % capaciteMax;
+        }
+
+		public : 
+
+		//Constructeur  
+		HashMap(size_t capacite) : taille(0), capaciteMax(capacite), tabHash(capacite) {};
+
+		// Fonction pour insérer un élément dans la table de hachage
+		bool put(const K& key, const V& value) {
+			size_t index = hash(key); //chercher l'index de l'élément avec la fonction de hachage 
+			std::forward_list<std::pair<K,V>>& liste = tabHash[index]; // Liste chaînée correspondante à l'index dans la table de hachage
+			bool inserer = false;
+
+			if(existe(key)) {
+				//parcourt les paires et met à jour la valeur 
+				for (std::pair<K,V>& paire : liste){
+					if(paire.first == key) {
+						paire.second = value;
+						inserer = true;
+						return inserer;
+					}
+				}
+                return false;
+			} else {
+				//créer une paire et l'insérer
+				std::pair<K,V> newP = make_pair(key,value);
+				liste.push_front(newP);
+                inserer = true;
+                return inserer;
+			}
+		}
+
+		//vérifier l'existence avec exists() avant d'utilier get
+		V get(const K& key) {
+			size_t index = hash(key);
+			std::forward_list<std::pair<K,V>>& liste = tabHash[index]; // Liste chaînée correspondante à l'index dans la table de hachage
+
+
+			for(const std::pair<K,V>& paire : liste) {
+				if(paire.first == key) {
+					return paire.second;
+				}
+			}
+
+            //cout << "None value found :/" << endl;
+            return 0; //aucune valeur n'a été trouvée 
+		}
+		
+		bool existe(const K& key) const {
+			size_t index = hash(key); // Chercher l'index de l'élément avec la fonction de hachage
+			const std::forward_list<std::pair<K, V>> & liste = tabHash[index]; // Liste chaînée correspondante à l'index dans la table de hachage
+
+			// Parcourir la liste pour vérifier si la clé existe
+			for (const std::pair<K, V>& paire : liste) {
+				if (paire.first == key) {
+					return true; // La clé existe
+				}
+			}
+
+			return false; // La clé n'existe pas
+		}
+
+		// Fonction pour obtenir le nombre d'élément de la table de hachage
+		size_t size() const {
+			return taille;
+		}
+
+        // méthode renvoie une référence  à la liste chaînée à un index donné (sinon jsp comment faire la 8)
+        vector<pair<K, V>> getAllPairs() const {
+            vector<pair<K, V>> pairs;
+            for (const forward_list<pair<K, V>>& list : tabHash) {
+                for (const pair<K, V>& pair : list) {
+                    pairs.push_back(pair);
+                }
+            }
+            return pairs;
+        }
+	
+
+	};
+
+}
+
+
+#endif
