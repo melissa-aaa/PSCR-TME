5491f91665adf4b7cd3dee281e292e3ca91bc6a8
28707210 <hana.mimouni@etu.sorbonne.universite.fr>
2023-12-08 15:54:32 +0100

    tme 8

--- TME8/src/ServerSocket.h
+++ TME8/src/ServerSocket.h
@@ -10,12 +10,38 @@ class ServerSocket {
 
 public :
 	// Demarre l'ecoute sur le port donne
-	ServerSocket(int port);
+	ServerSocket(int port){
+		fd = socket(AF_INET, SOCK_STREAM, 0);
+		if(!fd) {
+			perror("fd");
+			exit(-1);
+		}
+		sin.sin_family = AF_INET;
+		sin.sin_port = htons(port);
+		sin.sin_addr.s_addr = INADDR_ANY;
+		if(bind(fd, (struct sockaddr *) &sin, sizeof(sin))) {
+			close(fd);
+		}
+
+		listen(fd,50);
+		sockfd = fd;
+	}
 
 	int getFD() { return socketfd;}
 	bool isOpen() const {return socketfd != -1;}
 
-	Socket accept();
+	Socket accept(){
+		int fd = accept(socketfd, 0,0);
+		struct sockaddr_in addr;
+		socklen_t len = sizeof(addr);
+		if(fd = accept(socketfd, &addr, &len) == -1) {
+			perror("accept ");
+			exit(-1);
+		}
+
+		cout << "Connexion de " << addr << endl;
+		return fd;
+	}
 
 	void close();
 };
--- TME8/src/Socket.h
+++ TME8/src/Socket.h
@@ -15,13 +15,41 @@ public :
 	Socket(int fd):fd(fd){}
 
 	// tente de se connecter à l'hôte fourni
-	void connect(const std::string & host, int port);
-	void connect(in_addr ipv4, int port);
+	void Socket::connect(const std::string & host, int port){
+		struct addrinfo  hints;
+		memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+        hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+        hints.ai_flags = 0;
+        hints.ai_protocol = 0;          /* Any protocol */
+		stuct addrinfo* res; 
+
+		s = getaddrinfo(host, to_string(port), hints, &res);
+
+		if (s != 0) {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+            exit(EXIT_FAILURE);
+        }
+
+		fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if(!fd) {
+			perror("socket");
+			exit(-1);
+		}
+		connect(fd, res->ai_addr, res->ai_addrlen);
+		freeaddrinfo(res);
+
+	}
+	
 
 	bool isOpen() const {return fd != -1;}
 	int getFD() { return fd ;}
 
-	void close();
+	void close(){
+		shutdown(fd);
+		::close(fd);
+
+	}
 };
 
 std::ostream & operator<< (std::ostream & os, struct sockaddr_in * addr);
